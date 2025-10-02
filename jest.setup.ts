8429971af146592aa315a85915679627075651dd
Raphael Chappert <raphael.chappert@hotmail.fr>
2025-10-02 20:31:17 +0200

    test: add Jest setup for mocking Next.js navigation and cache functions

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,36 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfills for Next.js APIs
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
+
+// Mock Next.js navigation functions
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+      pathname: '/',
+      query: {},
+      asPath: '/',
+    };
+  },
+  usePathname() {
+    return '/';
+  },
+  useSearchParams() {
+    return new URLSearchParams();
+  },
+  notFound: jest.fn(),
+}));
+
+// Mock Next.js cache functions
+jest.mock('next/cache', () => ({
+  revalidatePath: jest.fn(),
+  revalidateTag: jest.fn(),
+}));
